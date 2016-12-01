3dd2c5e2e0be87c92d2ec0070908dc0c36dc0dba
Marty McLaughlin <martymclaugh@gmail.com>
2016-11-30 16:15:57 -0800

    add first and last name, and phone number to user migration

--- db/migrate/20161201001112_devise_create_users.rb
+++ db/migrate/20161201001112_devise_create_users.rb
@@ -4,6 +4,11 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.0]
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
+      t.string :access_token
+      t.string :first_name, null: false
+      t.string :last_name, null: false
+      t.string :phone_number, null: false
+
 
       ## Recoverable
       t.string   :reset_password_token
