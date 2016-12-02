abda220d8b37080294a63690e548d813620e3790
Marty McLaughlin <martymclaugh@gmail.com>
2016-12-02 14:35:47 -0800

    add users routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :user, only: []
-
+  resource :users
   namespace :v1, defaults: { format: :json } do
     resource :login, only: [:create], controller: :sessions
   end
