db95070822d05259083f6795d8fbda209b7de393
Marty McLaughlin <martymclaugh@gmail.com>
2016-12-02 17:22:45 -0800

    create phonenumber object when post route hits

--- app/controllers/phone_numbers_controller.rb
+++ app/controllers/phone_numbers_controller.rb
@@ -2,4 +2,12 @@ class PhoneNumbersController < ApplicationController
   def new
     @phone_number = PhoneNumbers.new
   end
+
+  def create
+    p params
+    @phone_number = PhoneNumber.find_or_create_by(phone_number: params[:phone_number])
+    @phone_number.create_pin
+    @phone_number.send_pin
+  end
+
 end
