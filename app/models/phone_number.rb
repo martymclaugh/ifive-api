70513588750abb7a1541804e6a7010ed56a8161d
Marty McLaughlin <martymclaugh@gmail.com>
2016-12-02 16:08:23 -0800

    add associations

--- app/models/phone_number.rb
+++ app/models/phone_number.rb
@@ -1,2 +1,3 @@
 class PhoneNumber < ApplicationRecord
+  belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,11 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  has_many :phone_numbers
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
   after_create :update_access_token!
-
-  validates :phone_number, presence: true
 
   def update_access_token!
     self.access_token = "#{self.id}:#{Devise.friendly_token}"
