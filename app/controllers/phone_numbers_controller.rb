a0855f63c0b32bfe5ac745fd14b830905863e5df
Marty McLaughlin <martymclaugh@gmail.com>
2016-12-04 14:59:22 -0800

    find or create phone number, send verification text with twilio

--- app/controllers/phone_numbers_controller.rb
+++ app/controllers/phone_numbers_controller.rb
@@ -1,15 +1,13 @@
 class PhoneNumbersController < ApplicationController
-  def new
-    @phone_number = PhoneNumbers.new
-  end
-
   def create
-    @phone_number = PhoneNumber.find_or_create_by(phone_number: params[:phone_number])
-    @phone_number.create_pin
-    @phone_number.send_pin
-  end
-
-  def verify
-    @phone_number = PhoneNumber.find_by(phone_number: params[])
+    p params
+    @phone = PhoneNumber.find_by(phone_number: params[:phone_number])
+    unless @phone
+      @phone = PhoneNumber.create(phone_number: params[:phone_number], user_id: params[:user_id], verified: false, pin: '')
+    end
+    @phone.create_pin
+    p 'pin created'
+    @phone.send_pin
+    p 'pin sent?'
   end
 end
