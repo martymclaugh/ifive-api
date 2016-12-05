aadc9f94b8d2044000a9686d24bf6dd99f240a03
Marty McLaughlin <martymclaugh@gmail.com>
2016-12-05 12:19:05 -0800

    send phone number with user object

--- app/controllers/v1/sessions_controller.rb
+++ app/controllers/v1/sessions_controller.rb
@@ -5,7 +5,6 @@ module V1
     def create
       @phone_number = PhoneNumber.where(phone_number: params[:phone_number])[0]
       @user = User.find(@phone_number.user_id)
-      @user_info = [@user, @phone_number]
       return invalid_login_attempt unless @user
       if @user.valid_password?(params[:password])
         sign_in(@user)
