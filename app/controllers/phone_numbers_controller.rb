ce7c7a927cb989f9286d972cd9bdcc1dba99108d
Marty McLaughlin <martymclaugh@gmail.com>
2016-12-04 16:15:38 -0800

    update verified boolean if code sent back was same as pin sent

--- app/controllers/phone_numbers_controller.rb
+++ app/controllers/phone_numbers_controller.rb
@@ -1,13 +1,23 @@
 class PhoneNumbersController < ApplicationController
+  skip_before_action :authenticate_user_from_token!
+
   def create
-    p params
     @phone = PhoneNumber.find_by(phone_number: params[:phone_number])
     unless @phone
       @phone = PhoneNumber.create(phone_number: params[:phone_number], user_id: params[:user_id], verified: false, pin: '')
     end
     @phone.create_pin
-    p 'pin created'
+    p @phone
     @phone.send_pin
-    p 'pin sent?'
+  end
+
+  def update
+    @phone = PhoneNumber.find_by(phone_number: params[:phone_number])
+    if params[:code] == @phone.pin
+      @phone.verification
+      render json: @phone
+    else
+      render json: { error: t('phone_verification_error') }, status: :unprocessable_entity
+    end
   end
 end
