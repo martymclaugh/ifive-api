f7d48ecaf1c2299ef5427df37fa5734d369bd767
Marty McLaughlin <martymclaugh@gmail.com>
2016-12-11 17:32:29 -0800

    add text migration

--- db/migrate/20161212010339_create_texts.rb
+++ db/migrate/20161212010339_create_texts.rb
@@ -1,7 +1,8 @@
 class CreateTexts < ActiveRecord::Migration[5.0]
   def change
     create_table :texts do |t|
-
+      t.integer :user_id, null: false
+      t.string :phone_number, null: false
       t.timestamps
     end
   end
