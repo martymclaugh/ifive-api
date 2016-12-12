aa3c9785fe7f153cfba3b277f4cb12326089f6c3
Marty McLaughlin <martymclaugh@gmail.com>
2016-12-11 17:04:22 -0800

    if theres a user with the phone number send push notification, otherwise send text

--- app/controllers/high_fives_controller.rb
+++ app/controllers/high_fives_controller.rb
@@ -2,25 +2,27 @@ class HighFivesController < ApplicationController
   def create
     GlobalPhone.db_path = Rails.root.join('db/global_phone.json')
     number_object = GlobalPhone.parse(params[:phone_number])
-    number = number_object.national_string
-    if number.length == 10
+    @number = number_object.national_string
+    if @number.length == 10
       @user = User.find(params[:user_id])
-      @high_five = HighFive.new(sender_id: params[:user_id], receiver_phone_number: number, giver_name: "#{@user.first_name} #{@user.last_name}", receiver_name: params[:friend_name])
+      @high_five = HighFive.new(sender_id: params[:user_id], receiver_phone_number: @number, giver_name: "#{@user.first_name} #{@user.last_name}", receiver_name: params[:friend_name])
       if @high_five.save
         @phone_number = PhoneNumber.find_by(phone_number: @high_five.receiver_phone_number)
+
         if @phone_number && @phone_number.verified
           # send push notification
-          render json: @high_five
-        else
           pem = File.join(Rails.root, 'certificates', 'ios_push_certificate.pem')
           apn = Houston::Client.development
           apn.certificate = File.read(pem)
-          token = "<#{@user.device_token}>"
+          token = "<#{@phone_number.user.device_token}>"
           notification = Houston::Notification.new(device: token)
           notification.alert = "#{@high_five.giver_name} sent you a Five! \u{270B}"
           apn.push(notification)
           puts "Error: #{notification.error}." if notification.error
+          render json: @high_five
+        else
           # send text
+          @user.phone_numbers[0].send_invite(@high_five.giver_name)
           render json: @high_five
         end
       else
