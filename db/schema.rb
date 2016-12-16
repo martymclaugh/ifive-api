f1ec970fb7ac84ad53a16f9e7d389a85e1ddc2ec
Marty McLaughlin <martymclaugh@gmail.com>
2016-12-15 17:52:06 -0800

    comment code out for itunes release

--- db/schema.rb
+++ db/schema.rb
@@ -46,7 +46,7 @@ ActiveRecord::Schema.define(version: 20161212010339) do
     t.string   "access_token"
     t.string   "first_name",                          null: false
     t.string   "last_name",                           null: false
-    t.string   "device_token",                        null: false
+    t.string   "device_token"
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
